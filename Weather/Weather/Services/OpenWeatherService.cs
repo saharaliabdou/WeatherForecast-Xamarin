fb6febe74562fed286ea6be6a766535766e70b59
Sahar Ali <sahar.aliabdou@gmail.com>
2022-04-07 13:42:34 +0200

    created forecast menu

--- Weather/Weather/Services/OpenWeatherService.cs
+++ Weather/Weather/Services/OpenWeatherService.cs
@@ -16,6 +16,76 @@ namespace Weather.Services
     public class OpenWeatherService
     {
 
+        public EventHandler<string> WeatherForecastAvailable;
+        HttpClient httpClient = new HttpClient();
+        readonly string apiKey = "7616850dd80ab0c5df57eb53e6668a2d"; // Your API Key
+
+        //part of your event code here
+        public async Task<Forecast> GetForecastAsync(string City)
+        {
+            //https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={City}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+            OnWeatherForecastAvailable($"New weather forecast available for {City} is avialble");
+            //part of your event code here
+
+            return forecast;
+
+        }
+        protected virtual void OnWeatherForecastAvailable(string s)
+        {
+            WeatherForecastAvailable?.Invoke(this, s);
+        }
+        public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
+        {
+            //https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+
+            //part of your event code here
+            OnWeatherForecastAvailable($"New weather forecast available for {longitude}{latitude} is avialble");
+            return forecast;
+        }
+        private async Task<Forecast> ReadWebApiAsync(string uri)
+        {
+            // part of your read web api code here
+
+            // part of your data transformation to Forecast here
+
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            WeatherApiData wd = await response.Content.ReadFromJsonAsync<WeatherApiData>();
+
+            //Your Code to convert WeatherApiData to Forecast using Linq.
+            Forecast forecast = new Forecast();
+
+            forecast.City = wd.city.name;
+
+
+            forecast.Items = new List<ForecastItem>();
+
+            wd.list.ForEach(wdListItem => { forecast.Items.Add(GetForecastItem(wdListItem)); });
+
+            return forecast;
+        }
+
+        private ForecastItem GetForecastItem(List wdListItem)
+        {
+
+            ForecastItem item = new ForecastItem();
+            item.DateTime = UnixTimeStampToDateTime(wdListItem.dt);
+
+            item.Temperature = wdListItem.main.temp;
+            item.Description = wdListItem.weather.Count > 0 ? wdListItem.weather.First().description : "No info";
+            item.WindSpeed = wdListItem.wind.speed;
+
+            return item;
+        }
+
         private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
             DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
--- Weather/Weather/Views/MainPageFlyout.xaml.cs
+++ Weather/Weather/Views/MainPageFlyout.xaml.cs
@@ -35,7 +35,20 @@ namespace Weather.Views
                 {
                     new MainPageFlyoutMenuItem { Id = 0, Title = "About Weather", TargetType=typeof(AboutPage) },
                     new MainPageFlyoutMenuItem { Id = 1, Title = "Debug Console", TargetType=typeof(ConsolePage) },
-                });
+                    new MainPageFlyoutMenuItem { Id = 2, Title = "Stockholm", TargetType=typeof(ForecastPage) },
+                    new MainPageFlyoutMenuItem { Id = 3, Title = "Uppsala", TargetType=typeof(ForecastPage) },
+                    new MainPageFlyoutMenuItem { Id = 4, Title = "New york", TargetType=typeof(ForecastPage) },
+                    new MainPageFlyoutMenuItem { Id = 5, Title = "Los Angelos", TargetType=typeof(ForecastPage) },
+                    new MainPageFlyoutMenuItem { Id = 6, Title = "Los Angelos", TargetType=typeof(ForecastPage) },
+                    new MainPageFlyoutMenuItem { Id = 7, Title = "Bombay", TargetType=typeof(ForecastPage) },
+                    new MainPageFlyoutMenuItem { Id = 8, Title = "Bangok", TargetType=typeof(ForecastPage) },
+                    new MainPageFlyoutMenuItem { Id = 9, Title = "São Paulo", TargetType=typeof(ForecastPage) },
+                    new MainPageFlyoutMenuItem { Id = 10, Title = "Buenos Aires", TargetType=typeof(ForecastPage) },
+                     
+            });
+
+               
+               
             }
         }
     }
