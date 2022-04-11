fe1327f1dab251baf26c0c15a88550bfd711faa7
Sahar Ali <sahar.aliabdou@gmail.com>
2022-04-11 09:23:59 +0200

    fixed layout on aboutPage

--- Weather/Weather/Views/AboutPage.xaml.cs
+++ Weather/Weather/Views/AboutPage.xaml.cs
@@ -13,7 +13,8 @@ namespace Weather.Views
         public AboutPage()
         {
             InitializeComponent();
-            sunnyGif.Source =  $"https://media0.giphy.com/media/vNg01outFUVhyKGiB2/giphy.gif";
+           // sunnyGif.Source =  $"https://media0.giphy.com/media/vNg01outFUVhyKGiB2/giphy.gif";
+           sunnyGif.Source =  $"https://cdn.dribbble.com/users/2277649/screenshots/8498294/media/1f87fae49becc4fac866d70cbb5eca37.gif";
         }
     }
 }
